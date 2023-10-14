866fbd71fb6df3dd3fecf0759ae18c9c70a6a97c
Luis Choque <luis.choque.castro@outlook.com>
2023-10-13 23:47:40 -0500

    Refactored github service

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,55 +1,35 @@
 import { GitHubRepository, GitHubBranch, GitHubCommit } from "../types/github.types";
 
+const BASE_URL: string = Bun.env.GITHUB_API_URL || "";
 
-const BASE_URL: string = Bun.env.GITHUB_API_URL ?? "";
+async function fetchJSON(url: string) {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data. Status: ${response.status}`);
+  }
+
+  const contentType = response.headers.get('content-type');
+  if (contentType?.includes('application/json')) {
+    return response.json();
+  } else {
+    throw new Error('Invalid response format: Expected JSON');
+  }
+}
 
 export default {
   async getRepositoryInformation(owner: string, repo: string): Promise<GitHubRepository> {
     const apiUrl = `${BASE_URL}/repos/${owner}/${repo}`;
-  
-    try {
-      const response = await fetch(apiUrl);
-      if (response.ok) {
-        const data = await response.json();
-        return data;
-      } else {
-        throw new Error('Unable to retrieve repository information');
-      }
-    } catch (error) {
-      throw new Error('Unable to retrieve repository information');
-    }
+    return fetchJSON(apiUrl);
   },
 
   async getBranches(owner: string, repo: string): Promise<GitHubBranch[]> {
     const apiUrl = `${BASE_URL}/repos/${owner}/${repo}/branches`;
-  
-    try {
-      const response = await fetch(apiUrl);
-      if (response.ok) {
-        const data = await response.json();
-        return data;
-      } else {
-        throw new Error('Unable to retrieve branches');
-      }
-    } catch (error) {
-      throw new Error('Unable to retrieve branches');
-    }
+    return fetchJSON(apiUrl);
   },
 
   async getCommits(owner: string, repo: string, sha: string): Promise<GitHubCommit[]> {
     const apiUrl = `${BASE_URL}/repos/${owner}/${repo}/commits?sha=${encodeURIComponent(sha)}`;
-
-    try {
-      const response = await fetch(apiUrl);
-      
-      if (response.ok) {
-        const data = await response.json();
-        return data;
-      } else {
-        throw new Error('Unable to retrieve commit history');
-      }
-    } catch (error) {
-      throw new Error('Unable to retrieve commit history');
-    }
+    return fetchJSON(apiUrl);
   },
 }

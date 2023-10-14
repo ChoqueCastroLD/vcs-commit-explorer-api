cf124c9c052db6ce5c27bc203cd46d841cbca818
Luis Choque <luis.choque.castro@outlook.com>
2023-10-14 15:49:29 -0500

    Added type definition por commits

--- src/controllers/commit.controller.ts
+++ src/controllers/commit.controller.ts
@@ -2,5 +2,17 @@ import GitHubService from "../services/github.service"
 
 
 export async function getCommits(vcs: string, owner: string, repository: string, sha: string) {
-    return GitHubService.getCommits(owner, repository, sha)
+    const commits = await GitHubService.getCommits(owner, repository, sha)
+    return commits.map(({ sha, node_id, parents, commit }) => {
+        return {
+            sha,
+            node_id,
+            message: commit.message,
+            author_name: commit.author.name,
+            author_email: commit.author.email,
+            commiter_name: commit.committer.name,
+            commiter_email: commit.committer.email,
+            parents: parents.map(({ sha }) => sha)
+        }
+    })
 }
--- src/routes/commits.routes.ts
+++ src/routes/commits.routes.ts
@@ -9,4 +9,24 @@ export const router = () => new Elysia()
         ({ params: { vcs, owner, repository, sha } }) => {
             return getCommits(vcs, owner, repository, sha)
         },
+        {
+            params: t.Object({
+                vcs: t.String(),
+                owner: t.String(),
+                repository: t.String(),
+                sha: t.String()
+            }),
+            response: t.Array(
+                t.Object({
+                    sha: t.String(),
+                    node_id: t.String(),
+                    message: t.String(),
+                    author_name: t.String(),
+                    author_email: t.String(),
+                    commiter_name: t.String(),
+                    commiter_email: t.String(),
+                    parents: t.Array(t.String())
+                })
+            )
+        }
     )

c455ad6653a33a24e39333823437c8c189cc9b5d
Luis Choque <luis.choque.castro@outlook.com>
2023-10-13 22:56:07 -0500

    Added inspect repository controller

---
+++ src/controllers/inspect.controller.ts
@@ -0,0 +1,21 @@
+import GithubService from "../services/github.service"
+
+
+export async function inspect(owner: string, repository: string) {
+    const repositoryInformation = await GithubService.getRepositoryInformation(owner, repository)
+    return {
+        owner: repositoryInformation.owner.login,
+        owner_avatar_url: repositoryInformation.owner.avatar_url,
+        owner_url: repositoryInformation.owner.url,
+        description: repositoryInformation.description,
+        stars: repositoryInformation.stargazers_count,
+        watchers: repositoryInformation.watchers_count,
+        language: repositoryInformation.language,
+        default_branch: repositoryInformation.default_branch,
+        license_name: repositoryInformation.license?.name,
+        license_url: repositoryInformation.license?.url,
+        fork: repositoryInformation.fork,
+        created_at: repositoryInformation.created_at,
+        updated_at: repositoryInformation.updated_at,
+    }
+}
